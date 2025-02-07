ebf195ced17b55b674a9f801918d8ddd6cf6b80d
Esmael Adriano <152002075+esmaeladriano@users.noreply.github.com>
2025-02-07 11:29:52 +0100

    Add files via upload

---
+++ Project/src/index.js
@@ -0,0 +1,34 @@
+  // Contagem Regressiva
+  const countDownDate = new Date("2024-12-15").getTime();
+
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("hours").textContent = hours
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("minutes").textContent = minutes
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("seconds").textContent = seconds
+      .toString()
+      .padStart(2, "0");
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "O evento começou!";
+    }
+  }
+
+  const x = setInterval(updateCountdown, 1000);
